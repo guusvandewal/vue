43f5b66c4f87b6a775569cfea06b8b590af1063c
Guus van de Wal <info@guusvandewal.nl>
2021-03-02 14:31:16 +0100

    Custom input ?

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
 import Form from "./components/Form.vue";
 import CustomButton from "./components/CustomButton.vue";
+import CustomInput from "./components/CustomInput.vue";
 export default {
   components: {
     Form,
     CustomButton,
+    CustomInput
   },
   data() {
     return {
@@ -24,9 +26,12 @@ export default {
   <img alt="Vue logo" src="./assets/logo.png" />
   <Form msg="This is a bootstrapped form" />
   <div class="move-down" :style="{ fontSize: postFontSize + 'em' }">
-    <CustomButton :msg="msg" @enlarge-text="postFontSize += 0.1" />
+    <CustomButton :msg="msg" @enlarge-text="postFontSize += $event" />
     <CustomButton msg="You really clicked me: " />
     <CustomButton title="You sure clickrred me: " />
+<!--    <custom-input v-model="searchText" @update:model-value="searchText = $event"></custom-input>
+    <custom-input v-model="searchText" :model-value="searchText" @update:model-value="resize"></custom-input>-->
+
   </div>
 </template>
 
