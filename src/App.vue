f92758bca4fc12a9d1a652b3dde7d9dbd5e1e9ca
Guus van de Wal <info@guusvandewal.nl>
2021-03-02 13:56:05 +0100

    Rewrite

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,19 @@
-<script setup>
+<script>
 import Form from "./components/Form.vue";
 import CustomButton from "./components/CustomButton.vue";
-import { ref } from '@vue/reactivity'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
-
-// Not possible with vue3?
-/*
 export default {
- components: CustomButton,
-  props: [
-    'msg',
-    'customProp'
-  ]
-}
-*/
-
-/*
-const onEnlargeText = (e) => {
-  l(e);
-}
-*/
-
-function onEnlargeText() {
-  console.log(this)
-  //this.postFontSize += enlargeAmount
-}
-
+  components: {
+    Form,
+    CustomButton,
+  },
+};
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <Form msg="This is a bootstrapped form" />
-  <div class="move-down" :style="{ fontSize: postFontSize += 0.2 }">
-    <CustomButton :msg=msg @enlarge-text="postFontSize += 0.2"/>
+  <div class="move-down" :style="{ fontSize: (postFontSize += 0.2) }">
+    <CustomButton :msg="msg" @enlarge-text="postFontSize += 0.2" />
     <CustomButton msg="You really clicked me: " />
     <CustomButton title="You sure clickrred me: " />
   </div>
