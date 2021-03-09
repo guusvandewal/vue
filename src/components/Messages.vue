ccaa93a1993a85e6b4cc1cd9bb7de6c2824fe9fa
Guus van de Wal <info@guusvandewal.nl>
2021-03-09 12:58:55 +0100

    Default to no teleport

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -1,11 +1,26 @@
 <template>
   <HelloWorld msg="Hello"></HelloWorld>
   <HelloWorld msg="Hi"></HelloWorld>
-  <teleport to="#modal">
+  <teleport to="#modal" :disabled='!showText'>
     <HelloWorld msg="Goodbye"></HelloWorld>
   </teleport>
+  <div>
+    This should be at the top.
+  </div>
+  <button @click="showText = !showText">
+    Toggle showText
+  </button>
 </template>
 
 <script setup>
 import HelloWorld from "./HelloWorld.vue";
+</script>
+<script>
+  export default {
+    data() {
+      return {
+        showText: false
+      };
+    }
+  };
 </script>
